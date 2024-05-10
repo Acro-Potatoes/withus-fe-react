460ec9dd32585ff927894b2910b90dd065fd88d7
KkamjangE <ajm980718@gmail.com>
2024-05-10 16:57:49 +0900

    Feat: 그룹메뉴 아이템 선택 색상 변경

--- src/layout/SideMenu/GroupMenuItem.tsx
+++ src/layout/SideMenu/GroupMenuItem.tsx
@@ -2,16 +2,28 @@ import { HashtagIcon } from '@heroicons/react/24/solid';
 import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
-const GroupMenuItem = ({
-    children,
-    to,
-}: {
+import {
+    useLayoutActions,
+    useLayoutSelectMenuState,
+} from '#/store/useLayoutStore';
+
+interface GroupMenuItemProps {
     children: ReactNode;
     to: string;
-}) => {
+    menuName: string;
+}
+
+const GroupMenuItem = ({ children, to, menuName }: GroupMenuItemProps) => {
+    const selectedMenu = useLayoutSelectMenuState();
+    const { setSelectedMenu } = useLayoutActions();
     return (
         <Link to={to}>
-            <button className='btn btn-ghost w-full justify-start text-lg font-medium'>
+            <button
+                onClick={() => setSelectedMenu(menuName)}
+                className={`btn btn-ghost w-full justify-start text-base font-medium md:text-lg ${
+                    selectedMenu === menuName && 'bg-neutral'
+                }`}
+            >
                 <HashtagIcon className='size-4' />
                 {children}
             </button>
--- src/layout/SideMenu/index.tsx
+++ src/layout/SideMenu/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 import GroupList from '#/layout/SideMenu/GroupList';
 import GroupMenu from '#/layout/SideMenu/GroupMenu';
 
 const SideMenu = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+    const handleClickMenuOpen = () => {
+        setIsMenuOpen((prev) => !prev);
+    };
     return (
-        <aside className='fixed mt-10 flex w-[300px]'>
-            <GroupList />
-            <GroupMenu />
+        <aside className='fixed z-10 mt-10 flex w-[300px]'>
+            <GroupList handleClickMenuOpen={handleClickMenuOpen} />
+            {isMenuOpen && <GroupMenu />}
         </aside>
     );
 };
