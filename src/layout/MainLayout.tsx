2314d110a7cc0c1a696983e36aa9e24f03d5c961
KkamjangE <ajm980718@gmail.com>
2024-05-10 16:51:21 +0900

    Style: 모바일 버전 UI 조정

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
                 <h1 className='text-base font-semibold'>WithUS</h1>
             </div>
             <div className='h-10' />
-            <div className='ml-[300px] h-full bg-neutral'>
+            <div className='bg-neutral md:ml-[300px] md:h-full'>
                 <Outlet />
             </div>
         </div>
