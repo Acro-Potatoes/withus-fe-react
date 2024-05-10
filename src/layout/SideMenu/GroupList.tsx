894a570a82001d17d2eb51bbec77712a4881d054
KkamjangE <ajm980718@gmail.com>
2024-05-10 16:57:06 +0900

    Feat: 그룹메뉴 모바일에서만 노출

--- src/layout/SideMenu/GroupList.tsx
+++ src/layout/SideMenu/GroupList.tsx
@@ -1,8 +1,13 @@
-import { SquaresPlusIcon } from '@heroicons/react/24/outline';
+import { ListBulletIcon, SquaresPlusIcon } from '@heroicons/react/24/outline';
+import { MobileView } from 'react-device-detect';
 
 import GroupItem from '#/layout/SideMenu/GroupItem';
 
-const GroupList = () => {
+const GroupList = ({
+    handleClickMenuOpen,
+}: {
+    handleClickMenuOpen: () => void;
+}) => {
     const groupList = [
         { groupName: 'A', id: '1' },
         { groupName: 'B', id: '2' },
@@ -12,6 +17,14 @@ const GroupList = () => {
     return (
         <div className='flex h-screen flex-col items-center bg-base-300'>
             <div className='mt-3 flex grow flex-col gap-5 p-3'>
+                <MobileView>
+                    <button
+                        onClick={handleClickMenuOpen}
+                        className='btn btn-square'
+                    >
+                        <ListBulletIcon />
+                    </button>
+                </MobileView>
                 {groupList.map((item, index) => (
                     <GroupItem key={index} groupItem={item} />
                 ))}
